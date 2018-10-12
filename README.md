bb1909e8b01895ee0923a0437088c40c22c55214
Léandre Daumont <1338620+digiz3d@users.noreply.github.com>
2018-10-12 22:56:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple automated setup
+
+Requirements : a fresh Ubuntu 18.04.1 install.
+
+In order to install the server :
+
+    sudo apt install git -y
+    git clone https://github.com/digiz3d/ydays-server-config.git
+    cd ydays-server-config
+    sudo bash setup.sh
---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+
+apt update
+apt upgrade -y
+apt install -y git build-essential libpcre3-dev libssl-dev zlib1g-dev
+
+cd
+
+git clone https://github.com/arut/nginx-rtmp-module.git
+
+wget https://nginx.org/download/nginx-1.15.5.tar.gz
+tar -xvzf nginx-1.15.5.tar.gz
+
+cd nginx-1.15.5
+./configure --add-module=/root/nginx-rtmp-module
+make
+make install
+
