f8431804274ab069c44c931bdef9ecf7f6c40902
Léandre Daumont <1338620+digiz3d@users.noreply.github.com>
2018-10-12 23:22:06 +0200

    should now start nginx

--- setup.sh
+++ setup.sh
@@ -17,8 +17,11 @@ git clone https://github.com/arut/nginx-rtmp-module.git
 wget https://nginx.org/download/nginx-1.15.5.tar.gz
 tar -xvzf nginx-1.15.5.tar.gz
 
-cd nginx-1.15.5
+cd ~/nginx-1.15.5
 ./configure --add-module=/root/nginx-rtmp-module
 make
 make install
 
+yes | cp ~/ydays-server-config/nginx.conf /usr/local/nginx/conf/nginx.conf
+yes | cp ~/ydays-server-config/nginx.service /lib/systemd/system/nginx.service
+systemctl start nginx
